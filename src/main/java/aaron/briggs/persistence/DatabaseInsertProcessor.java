5db3d5fb8c0115489534785d7cae3a2c95369674
Aaron Briggs <abriggs3@users.noreply.github.com>
2017-03-14 17:44:56 -0500

    Changed DatabaseInsertProcessor to use a prepared statement, which did not work.

--- src/main/java/aaron/briggs/persistence/DatabaseInsertProcessor.java
+++ src/main/java/aaron/briggs/persistence/DatabaseInsertProcessor.java
@@ -3,37 +3,32 @@ package aaron.briggs.persistence; /**
  */
 
 import java.sql.*;
+
 public class DatabaseInsertProcessor {
 
-
-
     public void insertData(String contentOfParagraph) {
-        PreparedStatement preparedContentOfParagraph = null;
 
         Database database = Database.getInstance();
         Connection connection = null;
-        try {
 
+        try {
             database.connect();
-            System.out.println("this is the contentOfParagraph " + contentOfParagraph);
             connection = database.getConnection();
-            Statement insertStatement = connection.createStatement();
             String sql;
             sql = "INSERT INTO paragraph (ID, dateParagraphPosted, timesFlaggedForContent, authorId, paragraphContent)" +
-                    " VALUES (null,null, 1, 1, 'contentOfParagraph')";
-
-            insertStatement.executeUpdate(sql);
+                    " VALUES (null,null, 1, 1,'" + contentOfParagraph + "')";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate(sql);
             database.disconnect();
+
         } catch (SQLException e) {
             System.out.println("SQL Exception has occurred");
             e.printStackTrace();
-
 
         } catch (Exception e) {
             System.out.println("General Exception has occurred");
             e.printStackTrace();
         }
-
     }
-
 }
+
